b977904d5a9aa1358e70c57372227b58f4c98839
Roufouanti <167672040+Roufouanti@users.noreply.github.com>
2024-04-21 10:27:16 +0200

    Add files via upload

---
+++ ObjetClass.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package objet;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class ObjetClass {
+    
+   ArrayList<String> listeObjet= new ArrayList <>();
+      
+   public ObjetClass(){
+       
+       listeObjet.add("clé à molette");
+       listeObjet.add("combinaison");
+       listeObjet.add("paire de gan de protection");
+       listeObjet.add("savon");
+       listeObjet.add("tablier intachable");
+       listeObjet.add("tortinette");
+       listeObjet.add("éxtinteur");
+       listeObjet.add("couteau");
+       listeObjet.add("blaster");
+       listeObjet.add("grénade");
+       listeObjet.add("médikit");
+       listeObjet.add("caméra");
+       listeObjet.add("souche de teste de mush");
+       listeObjet.add("débris métallique");
+       listeObjet.add("chat de schrödinger");
+       listeObjet.add("ration standard");
+       listeObjet.add("extracteur de spore");
+       listeObjet.add("sérum retro-fongique");
+       listeObjet.add("mycoscan");
+       listeObjet.add("lit");
+       listeObjet.add("douche");
+       listeObjet.add("jet d'exploration");
+       listeObjet.add("jet d'attaque");
+       listeObjet.add("terminal de recherche");
+       listeObjet.add("terminal");
+       listeObjet.add("reacteur BILGRED");   
+       listeObjet.add("gaz antispore");
+       listeObjet.add("sérum de constipaspore");
+       listeObjet.add("savon mushicide");
+       listeObjet.add("talki"); 
+   
+      
+          
+           
+    }
+   
+          
+    
+}
