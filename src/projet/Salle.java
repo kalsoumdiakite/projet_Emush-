87a989e27594107156e6722543cb2a4f83882c2f
Roufouanti <167672040+Roufouanti@users.noreply.github.com>
2024-04-24 10:00:09 +0200

    Add files via upload

--- src/projet/Salle.java
+++ src/projet/Salle.java
@@ -2,15 +2,38 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package projet;
+package objet;
 
 /**
  *
- * @author e0010u
+ * @author Utilisateur
  */
 public class Salle {
-    String nomSalle ;
-    Equipement [] list_equipemet;
-    //Objet[] espaceDeStockage;
+    private String nomSalle;
+    private Equipement [] list_equipemet;
+    private int id;
+    private String vaisseau;
     
+    public Salle(String nomSalle , int id , String vaisseau){
+        this.nomSalle = nomSalle;
+        this.id = id;
+        this.vaisseau = vaisseau;
+       
+    }
+    public void AjoutSalle(){
+    
+    }
+
+    public int getId(){
+     return id;
 }
+
+    public String getNomSalle(){
+    return nomSalle;
+            }
+    
+    public String vaisseau(){
+        return vaisseau;
+    }
+}
+    
