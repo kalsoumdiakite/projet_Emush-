dc52ec26441ec7d85d948da9872de4e21b442141
ArmyArmi <167771225+ArmyArmi@users.noreply.github.com>
2024-04-27 18:48:45 +0200

    Add files via upload

--- ObjetClass.java
+++ ObjetClass.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package objet;
+package tp;
 
 import java.util.ArrayList;
 
@@ -15,7 +15,8 @@ public class ObjetClass {
    ArrayList<String> listeObjet= new ArrayList <>();
       
    public ObjetClass(){
-       
+       //Création et ajout des objets dans la liste
+       listeObjet.add("Armure");
        listeObjet.add("clé à molette");
        listeObjet.add("combinaison");
        listeObjet.add("paire de gan de protection");
@@ -46,12 +47,44 @@ public class ObjetClass {
        listeObjet.add("sérum de constipaspore");
        listeObjet.add("savon mushicide");
        listeObjet.add("talki"); 
-   
-      
-          
-           
     }
+   public ArrayList<String> getListeObjet() {
+       listeObjet.add(0, "Absorbeun point de degat");
+       listeObjet.add(1, "Nécessaire dans l’inventaire pour réparer un equipement");
+       listeObjet.add(2, "Nécessaire dans l’inventaire pour piloter sinon mort");
+       listeObjet.add(3, "Nécessaire dans l'inventaire pour pas se salir");
+       listeObjet.add(4, "Côute un PA s'il est dans l'inventaire");
+       listeObjet.add(5, "Si c'est dans l'inventaire on se sali pas");
+       listeObjet.add(6, "Losrque qu'on converti PA en PM s'ajoute 2PM");
+       listeObjet.add(7, "Eteint l'incendie");
+       listeObjet.add(8, "Fait perde 2PV");
+       listeObjet.add(9, "Fait perdre 4PV");
+       listeObjet.add(10, "Fait perdre 6PV à tous les joueures de la salle");
+       listeObjet.add(11, "Doit etre dans l'inventaire pour soigner");
+       listeObjet.add(12, "Permet de voir les actions des mush et afficher les logs de la salle");
+       listeObjet.add(13, "Débloque les recherches si present dans la salle");
+       listeObjet.add(14, "Nécessaire pour reparer le vaisseau");
+       listeObjet.add(15, "Si un joueur le caresse il gagne un PMO par jour, Peut être contaminé par 1spore mush (ce qui augmente le nombre de spore sur les humains");
+       listeObjet.add(16, "Peuvent être cuisiner par Stephane dans le refectoire");
+       listeObjet.add(17, "Extrait un spore pour 1PA");
+       listeObjet.add(18, "Guerit un humain pour 2PA");
+       listeObjet.add(19, "Augmente la recherche de 3% à 10%");
+       listeObjet.add(20, "Pour se reposer");
+       listeObjet.add(21, "Être propre");
+       listeObjet.add(22, "Permet d'explorer d'autre planete");
+       listeObjet.add(23, "Permet de se defendre des attaques ennemis");
+       listeObjet.add(24, "Endroit où se fait les differents recherche");
+       listeObjet.add(25, "Endroit où on peut consulter tous ce qui cincerne le vaisseau");
+       listeObjet.add(26, "Réparer, ils pemettent de rentrer sur terre");
+       listeObjet.add(27, "Permet de reduire le nombre d'extraction de spores à 2 par jour");
+       listeObjet.add(28, "Augmente le côut de la production de spores à 2PA pour n mush");
+       listeObjet.add(29, "Permet d'extraire un spore losrqu'on prend une douche avec");
+       listeObjet.add(30, "Permet de comuniquer entre les joueurs");
+        return listeObjet;
+    }
+    
    
+       
+    }
           
     
-}
