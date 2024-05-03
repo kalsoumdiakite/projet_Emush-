2440e38894e92ffe0eb3f892e2e9c4f9e2a78151
Djibs123 <167713483+Djibs123@users.noreply.github.com>
2024-05-03 19:39:17 +0200

    Add files via upload

--- Competence.java
+++ Competence.java
@@ -1,28 +1,13 @@
-package projet;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package projet;
 
 /**
  *
  * @author Utilisateur
  */
-public class Competence {
-     private String nom;
-    private String description;
-
-    public Competence(String nom, String description) {
-        this.nom = nom;
-        this.description = description;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public String getDescription() {
-        return description;
-    }
+class Competence {
+    
 }
