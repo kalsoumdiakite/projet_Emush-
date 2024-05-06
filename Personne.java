abf5205385bbe7a2013e61276a55c87994b3db23
Roufouanti <167672040+Roufouanti@users.noreply.github.com>
2024-05-06 06:56:19 +0200

    Add files via upload

--- Personne.java
+++ Personne.java
@@ -5,6 +5,7 @@
 package Test;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -27,6 +28,9 @@ public class Personne {
     private String talkie_walkie= sc.nextLine() ;
     private ArrayList<String> canalHumain;
     private ArrayList<String> canalMush;
+    private Personne s;
+   // private boolean c;
+    
     
     public Personne(String nom, int PV, int PA, int PM, int PMO, boolean estMush, Competence competence1, Competence competence2) {
         this.nom = nom;
@@ -117,6 +121,9 @@ messages de ce canal dans la console. la gestion de la communication entre les j
     public void perdrePA(int points) {
         this.PA -= points;
     }
+    public void gagnerPMO(int points) {
+        this.PMO += points;
+    }
     public void perdrePM(int points) {
         this.PM -= points;
     }
@@ -168,14 +175,14 @@ messages de ce canal dans la console. la gestion de la communication entre les j
     System.out.println(this.nom+"vous pouvez prendre une douche");
     }   
     }
-    
+     // faire une recherche dans le laboratoire
      public void recherLabo(Personne joueur){ // methode pour faire une recherche dans la salle 
      if(joueur.getPA() >=2){
         this.perdrePA(2);
          System.out.println(this.nom +"fait une recherche");
      }
      else{
-                 System.out.println(this.nom+"PA insuffisant pour faire une recherche");           
+           System.out.println(this.nom+"PA insuffisant pour faire une recherche");           
     }
        
     }
@@ -187,7 +194,8 @@ messages de ce canal dans la console. la gestion de la communication entre les j
         } else{
         System.out.println(this.nom + "tu ne peux pas utiliser la tortinette");
         }
-    }
+    }   
+        //methode prendre un objet
       public void prendreObjet(Personne joueur , Objet objet , Salle salle){/* verifie si l'objet choisi est que veut prendre le joueur 
           est disponible dans dans l'espace de stockage*/
           if(salle.getStockage().contains("objet"){
@@ -197,16 +205,100 @@ messages de ce canal dans la console. la gestion de la communication entre les j
           else{
           
           System.out.println("l'objet n'est pas disponible dans l'unité de stockage de la salle ");}
-      
-      
       }
+
+      
+      // methode scanner
+     public void ScannerPlanete(){
+        if(this.salle.getnomSalle().equals("pont")&& this.PA>=3){// on verifie si le j es sur le pont et a assez de pa;
+            
+              
+              int s = (int)(Math.random()*3); //random , ici on choisi 1 secteur de la planete à scanner de façon aleatoir
+              int qtiteDeRessource = (int) (Math.random()*26);//e la quantité de ressources disponibles dans un des secteurs non scaner et choisi aleatoirement 
+                switch(s){
+                   case  1: 
+                     System.out.println("scane oxygne"+qtiteDeRessource+"unité");
+                   break;
+                   case  2 : 
+                     System.out.println("scane fuel"+qtiteDeRessource+"unité");
+                   break;
+                   case  3 : 
+                     System.out.println("scane débris métallique"+qtiteDeRessource+"unité");
+                   break;
+        }
+          this.perdrePA(3);//le joueur perd alors 3pa
+    }
+     }
+    
+     //methode fouiller dans l'unité de stockage
+     public void fouillerUniteStokage(Salle s){
+    //on va verifier si le j est present dans la salle et a de pa suffisantes pour fouiller 
+     if(s.presenceJoueur(this.s)&&this.PA>=2){
+        this.perdrePA(2);
+        
+        for(Objet Objet : salle.getStockage()){ // on va parcourir l'unité de stockage de la salle
+            boolean objetCache = false; // j'ai initialisé objetCache est faux s'il es pas cacher , 
+            if(objetCache = true){
+            System.out.printltn("affiche l'objet caché"+objet.getNom);}//pour l'afficher
+        }
+     }
+     else{
+          System.out.println("vous devez etre present dans la salle et avoirassez de pa pour pouvoir fouiler");
+        }
+       this.perdrePA(2);
+
+}     
+     //methode carresser le chat
+    public void carresserChat(){
+     if(inventaire.contains("chat de schrödinger")){
+      this.perdrePA(1);
+      if(Math.random()<0.5){  //  le j peut etre contaminé par 1spore / a une chance d'etre contaminer par une spore lorsqu'il carresse le chat
+      System.out.println("vous etes contaminé par un mush");
+       this.gagnerPMO(1);
+       }
+    }
+     else{
+         System.out.println("le chat n'est pas dans l'inventaire vous ne pouvez pas le carresser ");
+     }    
+    }
+    
+     // le joueur terminer son tour
+     public void termineTour(){
+         if(this.getPA()==0 && this.getPM()==0){
+         System.out.println("vous avez fini votre tour ");} 
+         else{
+             System.out.println("vous avez toojours de point d'action ou de mouvement ,");
+             }
+     }
+       
+     // changer salle 
+     public void changeSalle(Salle s){ // s : la salle dont le j veut changer
+         if(this.getPM()>=1){
+          if(s.salle.getSalleVoisines(s)){
+          s.ajouterJoueur(s);// ajoute le j dans la nouvelle salle
+          this.perdrePM(1);}
+          else{ System.out.println("vous avez chnger de salle"+s); }
+        }
+         else{ System.out.println("vous avez pas assezde PM pour chnger de salle ");}
+     }
+       // methode detecter la planete à proximité
+     public void detecterPlanetProximite(){
+         if(this.salle.getnomSalle().equals("pont")&& this.PA>=2){ // on va verifier d'abord si le joueur est au pont et a suffisement de pa
+             
+    
+     }
+     
+    }
+     // pour Eleesha 
+      public void consulterEntreeSortie(){
+     
+                                          }
+      /*se coucher 
+     public void seCoucher(){}*/
+    
     /*public void creerSpores(){//verifie d'abord si lejoueur est  mush et est seul dans la salle , il perd 2pa et devient sale
         if(this.estMush) &&(!this.estMush).prenceJoueur(this.salle){
         this.perdrePA(2);
-        
-    }
-       
-    }
     */
  
     //À tout moment, chaque joueur sait combien il y a de mushs vivants à bord.
