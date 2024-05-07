d4e2ad8caa68a406c4162ba644ac0bcc169118ff
kalsoumdiakite <118550297+kalsoumdiakite@users.noreply.github.com>
2024-05-07 17:17:12 +0200

    Add files via upload

--- src/projet/Projet.java
+++ src/projet/Projet.java
@@ -14,7 +14,16 @@ public class Projet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+       /*L’ensemble des objets est réparti aléatoirement dans les différentes salles du vaisseau au
+démarrage du jeu, à l’exception des rations de nourriture entreposées dans le réfectoire. */
+        Vaisseau v_Spatial = new Vaisseau();
+        String [] tab1= {"Pont","Tour.A", "Tour.B","Coul" , "Labo", "Jardin"};
+        boolean [][]tab2 = {{false ,true,true,true,false,false},{true ,false,false,true,false,false},{true ,false,false,true,false,false},{true,true,true,false,true,true},{false ,false,false,true,false,false},{false ,false,false,true,false,false}};
+        v_Spatial.init(tab1,tab2);
+        System.out.println(v_Spatial.toString());
+        System.out.println(v_Spatial.getIDVaisseau());
     }
+    
     
 }
